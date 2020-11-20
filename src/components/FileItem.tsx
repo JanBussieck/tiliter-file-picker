a5683faef3c946725175ac35d3d56134e3004edc
Jan Bussieck <jan.bussieck@gmail.com>
2020-11-20 21:04:21 +0100

    Add progress indicator to file item

--- src/components/FileItem.tsx
+++ src/components/FileItem.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import {
   ListItem,
   ListItemText,
   ListItemAvatar,
   Avatar,
+  CircularProgress,
+  Fab,
+  colors,
 } from '@material-ui/core';
-import {YouTube, Description, Image} from '@material-ui/icons';
+import {YouTube, Description, Image, Check} from '@material-ui/icons';
 import humanizeFileSize from '../utils/humanizeFileSize';
+import upload from '../services/uploadService';
 
 import type File from '../types/FileInterface';
 
@@ -21,12 +26,41 @@ const fetchIcon = (fileType: string) => {
   return Description;
 };
 
+const useStyles = makeStyles(theme => ({
+  successButton: {
+    backgroundColor: colors.green[500],
+    color: 'white',
+  },
+}));
+
 interface FileItemProps {
   file: File;
   id: string | number;
 }
 
 const FileItem: React.FC<FileItemProps> = ({file}) => {
+  const classes = useStyles();
+  const [completed, setCompleted] = React.useState(0);
+  const [success, setSuccess] = React.useState(false);
+
+  // start upload on component mount
+  useEffect(() => {
+    const cancel = upload({
+      file,
+      progress: progress => {
+        setCompleted(progress);
+      },
+      success: ({file}) => {
+        setSuccess(true);
+        console.log('success');
+      },
+    });
+
+    // useEffect clean up function will
+    // cancel upload if component is unmounted
+    return cancel;
+  }, []);
+
   const IconComponent = fetchIcon(file.type);
   return (
     <ListItem>
@@ -39,6 +73,13 @@ const FileItem: React.FC<FileItemProps> = ({file}) => {
         primary={file.name}
         secondary={humanizeFileSize(file.size)}
       />
+      {success ? (
+        <Fab size='small' className={classes.successButton}>
+          <Check />
+        </Fab>
+      ) : (
+        <CircularProgress variant="static" color="primary" value={completed} />
+      )}
     </ListItem>
   );
 };

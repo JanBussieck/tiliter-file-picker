4607791c4260a51c625a01fd0984e1361f77f633
Jan Bussieck <jan.bussieck@gmail.com>
2020-11-20 12:08:46 +0100

    Add materialize theme for Tiliter CI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import {ThemeProvider} from '@material-ui/styles';
+import {createMuiTheme} from '@material-ui/core/styles';
 import './App.css';
+
+const palette = {
+  primary: {main: '#32acf1'},
+  secondary: {main: '#f8b195'},
+};
+
+const themeName = 'Tiliter Main CI';
+
+const theme = createMuiTheme({palette, themeName});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
